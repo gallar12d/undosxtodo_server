f3e7e6637a95bbd2863cde0148e62a2bd8c75dcc
Julian Ortiz <julian-ortiz98@hotmail.com>
2023-04-17 18:52:51 -0500

    cors option7

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,13 @@ var corsOptions = {
     origin: "https://ultimilla.com/",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(async (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://ultimilla.com"); // update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(router);
 db().then(() => console.log("Conexion Ready"));
