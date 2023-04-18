85a6b49a0347019c0322fd0053488099add396b9
Julian Ortiz <julian-ortiz98@hotmail.com>
2023-04-17 19:52:19 -0500

    cors options9

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,13 @@ import db from "./infrastructure/db/mongo";
 const PORT = process.env.PORT || 3000;
 // const PORT = 3000;
 const app = express();
-app.use(cors());
-// app.use(async (req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://ultimilla.com"); // update to match the domain you will make the request from
-//     // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+// app.use(cors());
+app.use(async (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://ultimilla.com"); // update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(router);
 db().then(() => console.log("Conexion Ready"));
